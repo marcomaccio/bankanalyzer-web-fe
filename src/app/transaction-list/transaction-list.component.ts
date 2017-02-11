726dc85905f7e3d2336f029e2fe4162ef9fe25de
Marco Maccio <marco.maccio@marmac.name>
2017-02-11 22:32:09 +0100

    Added correct import for future development

--- src/app/transaction-list/transaction-list.component.ts
+++ src/app/transaction-list/transaction-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { Transaction }        from '../transaction';
+
 
 @Component({
   selector:     'app-transaction-list',
